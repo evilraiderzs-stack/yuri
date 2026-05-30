fb51f49054c736620385e2e2ab41b7ae0f058364
vboxuser <evilraiderzs@gmail.com>
2026-05-30 03:44:34 +0000

    x

--- src/BaiheTale.lua
+++ src/BaiheTale.lua
@@ -1,8 +1,12 @@
--- {"id":12322,"ver":"0.1.0","libVer":"1.0.0","author":"YourName"}
+-- {"id":12322,"ver":"0.2.0","libVer":"1.0.0","author":"YourName","dep":["WPCommon>=1.0.0"]}
 
 local baseURL = "https://baihetales.wordpress.com"
 
 local WPCommon = Require("WPCommon")
+
+----------------------------------------------------
+-- URL HELPERS
+----------------------------------------------------
 
 local function shrinkURL(url)
     return url:gsub("^.-baihetales%.wordpress%.com", "")
@@ -24,48 +28,82 @@ local function parsePage(url)
         return nil
     end
 
-    WPCommon.cleanupElement(content)
-    WPCommon.cleanupPassages(content:children())
+    ------------------------------------------------
+    -- Remove everything after first HR
+    -- (Patreon, navigation, socials, etc.)
+    ------------------------------------------------
 
-    -- Remove everything after the translation text
-    map(content:select("hr"), function(hr)
-        hr:remove()
-    end)
+    local hr = content:selectFirst("hr")
 
-    map(content:select(".wp-block-columns"), function(e)
-        e:remove()
-    end)
+    if hr then
+        local elem = hr
+
+        while elem do
+            local nextElem = elem:nextElementSibling()
+            elem:remove()
+            elem = nextElem
+        end
+    end
+
+    ------------------------------------------------
+    -- Remove scripts
+    ------------------------------------------------
 
     map(content:select("script"), function(e)
         e:remove()
     end)
 
+    ------------------------------------------------
+    -- Remove iframes
+    ------------------------------------------------
+
+    map(content:select("iframe"), function(e)
+        e:remove()
+    end)
+
+    ------------------------------------------------
+    -- Remove ads
+    ------------------------------------------------
+
+    map(content:select("[id^=atatags]"), function(e)
+        e:remove()
+    end)
+
+    map(content:select("#jp-post-flair"), function(e)
+        e:remove()
+    end)
+
+    WPCommon.cleanupElement(content)
+    WPCommon.cleanupPassages(content:children())
+
     return content
 end
 
 ----------------------------------------------------
--- NOVEL LIST
+-- LISTINGS
 ----------------------------------------------------
 
 local function findListings(doc)
     local novels = {}
 
-    map(doc:select(".wp-block-post-template li"), function(item)
-        local titleElem = item:selectFirst(".wp-block-post-title a")
+    map(doc:select("li.wp-block-post"), function(post)
+        local titleElem = post:selectFirst("h3.wp-block-post-title a")
 
-        if titleElem then
-            novels[#novels + 1] = Novel {
-                title = titleElem:text(),
-                link = shrinkURL(titleElem:attr("href"))
-            }
+        if not titleElem then
+            return
         end
+
+        novels[#novels + 1] = Novel {
+            title = titleElem:text(),
+            link = shrinkURL(titleElem:attr("href"))
+        }
     end)
 
     return novels
 end
 
 ----------------------------------------------------
--- NOVEL PAGE / TOC
+-- NOVEL PARSER
 ----------------------------------------------------
 
 local function parseNovelPage(novelURL, loadChapters)
@@ -77,25 +115,49 @@ local function parseNovelPage(novelURL, loadChapters)
         title = article:selectFirst(".entry-title"):text()
     }
 
+    ------------------------------------------------
+    -- COVER
+    ------------------------------------------------
+
     local img = article:selectFirst(".entry-content img")
+
     if img then
         info:setImageURL(img:attr("src"))
     end
 
+    ------------------------------------------------
+    -- CHAPTERS
+    ------------------------------------------------
+
     if loadChapters then
         local chapters = {}
 
-        local content = article:selectFirst(".entry-content")
+        local page = 1
 
-        if content then
-            map(content:select("a"), function(a)
+        while true do
+            local pageURL = novelURL
+
+            if page > 1 then
+                pageURL = novelURL .. "?query-4-page=" .. page
+            end
+
+            local pageDoc = GETDocument(expandURL(pageURL))
+
+            local links = pageDoc:select(
+                "li.wp-block-post h6.wp-block-post-title a"
+            )
+
+            if links:isEmpty() then
+                break
+            end
+
+            map(links, function(a)
                 local href = a:attr("href")
 
-                if href
-                    and href:find("baihetales.wordpress.com")
-                    and not href:find("patreon")
-                    and not href:find("buymeacoffee")
-                then
+                if WPCommon.contains(
+                    href,
+                    "baihetales.wordpress.com"
+                ) then
                     chapters[#chapters + 1] = NovelChapter {
                         order = #chapters + 1,
                         title = a:text(),
@@ -103,6 +165,8 @@ local function parseNovelPage(novelURL, loadChapters)
                     }
                 end
             end)
+
+            page = page + 1
         end
 
         info:setChapters(AsList(chapters))
@@ -120,6 +184,7 @@ return {
     name = "Baihe Tales",
     baseURL = baseURL,
     imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/Tintan.png",
+
     hasSearch = false,
     chapterType = ChapterType.HTML,
 
@@ -141,3 +206,4 @@ return {
     shrinkURL = shrinkURL,
     expandURL = expandURL
 }
+```
